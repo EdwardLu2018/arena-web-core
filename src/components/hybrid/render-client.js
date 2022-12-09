cf1da4c8e0199e800028a1906cd8a68d3d7c205f
Mathias Thomas <mathiasthomas@mathiass-air.wifi.local.cmu.edu>
2022-12-09 14:34:26 -0500

    Checking abort

--- src/components/hybrid/render-client.js
+++ src/components/hybrid/render-client.js
@@ -61,7 +61,7 @@ AFRAME.registerComponent('render-client', {
         };
 
         this.connectToCloud();
-        document.querySelector('a-scene').setAttribute('remote-render',{enabled: true});
+       // document.querySelector('a-scene').setAttribute('remote-render',{enabled: true});
         window.addEventListener('hybrid-onremoterender', this.onRemoteRender.bind(this));
         console.log('[render-client]', this.id);
         console.log(document.querySelector('a-scene').systems);
